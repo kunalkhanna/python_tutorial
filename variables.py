73fec886852492680c9e8f388172b86ffca49c91
kunalk7 <kunalk7@hotmail.com>
2016-02-19 13:41:45 -0800

    add variables.py file.

--- variables.py
+++ variables.py
@@ -7,3 +7,12 @@ name    = "John"       # A string
 print (counter)
 print (miles)
 print (name)
+
+str = 'Hello World!'
+
+print (str)          # Prints complete string
+print (str[0])       # Prints first character of the string
+print (str[2:5])     # Prints characters starting from 3rd to 5th
+print (str[2:])      # Prints string starting from 3rd character
+print (str * 2)      # Prints string two times
+print (str + "TEST") # Prints concatenated string
